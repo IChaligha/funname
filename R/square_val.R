a4010663fed36df2bfed2b9ade7fbb177a797847
IChaligha <ibrahim.chaligha@tigo.co.tz>
2022-11-18 16:39:54 +0300

    first commit

--- R/square_val.R
+++ R/square_val.R
@@ -1,8 +1,18 @@
 
+library(devtools)
+library(usethis)
 
+#' Title
+#'
+#' @param x A numeric va lue
+#'
+#' @return A number
+#' @export
+#'
+#' @examples
+#' square_val(2)
 square_val <- function(x) {
   x^2
 }
-
 
 
